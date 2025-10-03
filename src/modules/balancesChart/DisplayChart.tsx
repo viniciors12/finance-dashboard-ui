5446308fbb99cb2aa8b215239109e69a860d0a5a
viniciors12 <viniciorodriguez97@gmail.com>
2025-10-03 16:55:37 -0600

    fix

--- src/modules/balancesChart/DisplayChart.tsx
+++ src/modules/balancesChart/DisplayChart.tsx
@@ -6,19 +6,22 @@ import { useTransactionsContext } from "context/TransactionsContext";
 import { useFormReducer } from "hooks/FormReducer";
 import type { ChartFilterRequest, ChartFilterResponse } from "@models";
 import dayjs from "dayjs";
+const initialState: ChartFilterRequest = {
+  fromDate: dayjs().startOf("month").toDate(),
+  toDate: dayjs().endOf("month").toDate(),
+  category: "All",
+};
 
 export const DisplayChart = () => {
   const { fetchFilteredTransactions } = useTransactionsContext();
   const { form, onValueChanged, hasChanged } =
-    useFormReducer<ChartFilterRequest>({
-      fromDate: dayjs().startOf("month").toDate(),
-      toDate: dayjs().endOf("month").toDate(),
-      category: "All",
-    });
+    useFormReducer<ChartFilterRequest>(initialState);
   const [chartData, setChartData] = useState<ChartFilterResponse[]>([]);
 
   useEffect(() => {
-    if (hasChanged) {
+    const isInitialState = JSON.stringify(form) == JSON.stringify(initialState);
+
+    if (hasChanged || isInitialState) {
       fetchFilteredTransactions(form).then((data) => setChartData(data ?? []));
     }
   }, [form, hasChanged]);
